d037996345c5e84b25b724a857d1e4b9a4355f29
Isleimar Oliveira <isleimar@hotmail.com>
2019-07-14 18:41:49 -0300

    Inciando a criação ListView de notícias

--- getCzNew/app/src/main/java/com/example/getcznews/screens/TelaPrincipal.java
+++ getCzNew/app/src/main/java/com/example/getcznews/screens/TelaPrincipal.java
@@ -1,7 +1,11 @@
 package com.example.getcznews.screens;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.getcznews.TextEdit;
 
@@ -12,11 +16,21 @@ public class TelaPrincipal extends TelaModeloAtivo {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //USUARIO
-        TextEdit teUsuario = new TextEdit(
-                this,
-                getRoot(),
-                "Usuário"
-        );
+        //TITULO
+        TextView tvTitulo = new TextView(getRoot().getContext());
+        tvTitulo.setText("Lista de Notícias");
+        getRoot().addView(tvTitulo);
+
+        //LISTVIEW
+
+        ListView lvNoticias = new ListView(getRoot().getContext());
+
+        String[] dados = {"Notícia 1", "Notícia 2", "Notícia 3", "Notícia 4", "Notícia 5"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dados);
+        lvNoticias.setAdapter(adapter);
+
+        getRoot().addView(lvNoticias);
+
     }
 }
