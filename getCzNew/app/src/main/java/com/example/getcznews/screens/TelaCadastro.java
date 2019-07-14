11b5a87f61229d2a4b528af030d00cb144ac8397
antonio <antoniofilhonerde@gmail.com>
2019-07-14 20:45:34 -0300

    tela de cadastro

---
+++ getCzNew/app/src/main/java/com/example/getcznews/screens/TelaCadastro.java
@@ -0,0 +1,79 @@
+package com.example.getcznews.screens;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.getcznews.TextEdit;
+
+public class TelaCadastro extends TelaModeloInativo {
+
+    public TelaCadastro(){}
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        soLogado=false;
+
+        final LinearLayout root = new LinearLayout(this);
+        root.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                )
+        );
+
+        root.setBackgroundColor(Color.WHITE);
+        root.setOrientation(LinearLayout.VERTICAL);
+        setContentView(root);
+
+        //TITULO
+        TextView tvTitulo = new TextView(root.getContext());
+        tvTitulo.setText("GetNews");
+        tvTitulo.setTextSize(40);
+        tvTitulo.setGravity(Gravity.CENTER_HORIZONTAL);
+        root.addView(tvTitulo);
+
+        //CADASTRO
+        TextView tvCadastro = new TextView(root.getContext());
+        tvCadastro.setText("Cadastre-se");
+        tvCadastro.setGravity(Gravity.CENTER_HORIZONTAL);
+        root.addView(tvCadastro);
+
+        //NOME
+        TextEdit tvNome = new TextEdit(this,root,"Nome");
+
+        //USUARIO
+        TextEdit tvUser = new TextEdit(this,root,"Usuário");
+
+        //SENHA
+        TextEdit tvSenha = new TextEdit(this,root,"Senha");
+        tvSenha.getEt().setTransformationMethod(new PasswordTransformationMethod());
+
+        //LAYOUT DOS BOTÕES
+        LinearLayout llBotoes = new LinearLayout(root.getContext());
+        llBotoes.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+        );
+        root.addView(llBotoes);
+
+        Button btCadastrar = new Button(llBotoes.getContext());
+        btCadastrar.setText("Cadastrar");
+        llBotoes.addView(btCadastrar);
+
+
+
+
+    }
+}
