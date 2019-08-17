db9a280b2cffea53a63189e4ba92248d2b0d43cc
isleimar <isleimar@gmail.com>
2019-08-17 14:34:04 -0300

    Modificando NoticiaService

--- getCzNew/app/src/main/java/com/example/getcznews/services/NoticiaService.java
+++ getCzNew/app/src/main/java/com/example/getcznews/services/NoticiaService.java
@@ -1,4 +1,32 @@
 package com.example.getcznews.services;
 
+import android.content.Context;
+
+import com.example.getcznews.dao.NoticiaDAO;
+import com.example.getcznews.dao.NoticiaDAOImpl;
+import com.example.getcznews.domain.Noticia;
+
+import java.util.List;
+
 public class NoticiaService {
+
+    private NoticiaDAO noticiaDAO;
+
+    public NoticiaService(Context context) {
+        noticiaDAO = new NoticiaDAOImpl(context);
+    }
+
+    public List<Noticia> getListaNoticias(){
+        return noticiaDAO.listar();
+    }
+
+    public Noticia getNoticiaId(long id){
+        Noticia noticia = noticiaDAO.buscar(id);
+
+        if (noticia == null)
+            noticia = new Noticia();
+
+        return noticia;
+
+    }
 }
