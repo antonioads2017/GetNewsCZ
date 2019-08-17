627ac92a607a2ca3c195b21ab0a819d91cf9c9a5
isleimar <isleimar@gmail.com>
2019-08-17 14:33:06 -0300

    Modificando a tela principal

--- getCzNew/app/src/main/java/com/example/getcznews/screens/TelaPrincipal.java
+++ getCzNew/app/src/main/java/com/example/getcznews/screens/TelaPrincipal.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import com.example.getcznews.adapter.AdapterNoticiaPersonalizado;
 import com.example.getcznews.domain.Noticia;
+import com.example.getcznews.services.NoticiaService;
 import com.example.getcznews.services.TimeView;
 import com.example.getcznews.services.TimeWeb;
 
@@ -71,19 +72,21 @@ public class TelaPrincipal extends TelaModeloAtivo {
         //Atualizando o titulo da lista de notícias
         tvTitulo.setText(TITULO_LISTA_NOTICIA + getDataComoString());
 
-        List<Noticia> noticias = new ArrayList<>();
+        List<Noticia> noticias = new NoticiaService(this).getListaNoticias();
 
-        for (int x = 0; x < 13; ++x){
-            noticias.add(
-                    new Noticia(
-                            x,
-                            "Título "+ String.valueOf(x +1) + " " + Long.toString(System.currentTimeMillis()),
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique luctus magna eget iaculis. Pellentesque scelerisque orci ultrices elementum bibendum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas purus sapien, vestibulum eget tincidunt ac, finibus vitae est. Quisque bibendum condimentum arcu, vel facilisis eros fringilla quis. Aliquam sed nibh ut mauris ultrices ullamcorper quis a ante. Maecenas in mauris dictum, mollis lectus id, fermentum est. Vivamus dictum ornare nunc sed condimentum. Quisque vel lacus diam. Quisque sem elit, auctor sit amet nisl a, accumsan tincidunt felis.",
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKj3fglpYyBCh6XAUkSCFXbX8x5yi7vHg-XCSv06Xr_XqEoCzk",
-                            "https://g1.globo.com/",
-                            false)
-            );
-        }
+//        List<Noticia> noticias = new ArrayList<>();
+//
+//        for (int x = 0; x < 13; ++x){
+//            noticias.add(
+//                    new Noticia(
+//                            x,
+//                            "Título "+ String.valueOf(x +1) + " " + Long.toString(System.currentTimeMillis()),
+//                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique luctus magna eget iaculis. Pellentesque scelerisque orci ultrices elementum bibendum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas purus sapien, vestibulum eget tincidunt ac, finibus vitae est. Quisque bibendum condimentum arcu, vel facilisis eros fringilla quis. Aliquam sed nibh ut mauris ultrices ullamcorper quis a ante. Maecenas in mauris dictum, mollis lectus id, fermentum est. Vivamus dictum ornare nunc sed condimentum. Quisque vel lacus diam. Quisque sem elit, auctor sit amet nisl a, accumsan tincidunt felis.",
+//                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKj3fglpYyBCh6XAUkSCFXbX8x5yi7vHg-XCSv06Xr_XqEoCzk",
+//                            "https://g1.globo.com/",
+//                            false)
+//            );
+//        }
 
         AdapterNoticiaPersonalizado adapter = new AdapterNoticiaPersonalizado(noticias,this);
         lvNoticias.setAdapter(adapter);
